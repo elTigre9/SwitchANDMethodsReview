39a98d3b7740651e0b0288e004104b50387eff15
elTigre9 <33560876+elTigre9@users.noreply.github.com>
2019-05-29 14:31:21 -0500

    Add files via upload

---
+++ hwReview528/hwReview528/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace hwReview528
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello adventurer");
+            Console.WriteLine("Choose your weapon:");
+            Console.WriteLine("1. Sword, 2. Deagle, 3. Laptop: ");
+            string weapon = Console.ReadLine();
+
+            switch(weapon)
+            {
+                case "1":
+                    attackSword();
+                    break;
+                case "2":
+                    attackDeagle(99);
+                    break;
+                case "3":
+                    string enemyResult = attackLaptop(8);
+                    Console.WriteLine(enemyResult);
+                    break;
+                default:
+                    Console.WriteLine("You died for lack of weapon. Rot in hell!");
+                    break;
+            }
+        }
+
+        private static void attackSword()
+        {
+            Console.WriteLine("You chose sword");
+            Console.WriteLine("You attacked with the sword");
+        }
+
+        private static void attackDeagle(int damage)
+        {
+            Console.WriteLine("You chose Deagle");
+            Console.WriteLine("You attacked with Deagle for {0} dmg.", damage);
+        }
+
+        private static string attackLaptop(int looper)
+        {
+            Console.WriteLine("You chose laptop");
+            for(int i = 0; i < looper; i++)
+            {
+                attackDeagle(i * 40);
+            }
+
+            return "Your enemy is definitely vanquished";
+        }
+    }
+}
